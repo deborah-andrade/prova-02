68ab523d6ffdc3c7f416b3b04280852068b4e3ae
Déborah Andrade <104578483+deborah-andrade@users.noreply.github.com>
2022-06-27 01:26:32 -0300

    Update questão 02a).py

--- questão 02a).py
+++ questão 02a).py
@@ -1,9 +1,9 @@
 b = input("Digite um número em Binario: ")
 
 
-def binaryToDecimal(n):
-    return int(n, 2)
+def binarioparadecimal(n):    #defini a função.
+    return int(n, 2)          #fiz um retorno para voltar como inteiro.
 
 
-if __name__ == '__main__':
-    print('O resultado será:', binaryToDecimal(b))
+if __name__ == '__main__':                  #usei if __name__ com ajuda de aulas e sites explicativos.
+    print('O resultado será:', binarioparadecimal(b))
