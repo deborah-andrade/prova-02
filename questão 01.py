39801dd0292acc87f732ad158acedcb23a135cb6
Déborah Andrade <104578483+deborah-andrade@users.noreply.github.com>
2022-06-27 01:23:11 -0300

    Update questão 01.py

--- questão 01.py
+++ questão 01.py
@@ -1,10 +1,10 @@
-x = input('Digite uma frase: ')
+x = input('Digite uma frase: ') 
 
-from random import choice
+from random import choice #impotei da biblioteca random so o que eu queria usar, que no caso era choice.
 
 
-def StringMaluca(texto):
-    print(''.join(choice((str.upper, str.lower))(char) for char in texto))
-StringMaluca(x)
+def StringMaluca(texto):     #criei a função e a defini
+    print(''.join(choice((str.upper, str.lower))(char) for char in texto))  #usei choice para fazer a escolha das letras, usei as funções de str e por fim
+StringMaluca(x)                                                              #usei char para percorer pela frase escolhida pela estrutura for.
 StringMaluca(x) 
 StringMaluca(x)
